2498a4cdb3b6ced0a97700a71669d3d9909c3f32
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-17 17:20:26 +0300

    JS variables declared

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
